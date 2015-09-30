ee53679ced8cc97d5b8240c54f333c6e48df2f3e
Brian Kennedy <briankennedy1@gmail.com>
2015-09-30 15:27:35 -0700

    More cleanup

--- viz.js
+++ viz.js
@@ -8,9 +8,11 @@
   var svg = d3.select("body").append("svg");
 
   svg.attr("height", canvasHeight)
-      .attr("width", canvasWidth);
+     .attr("width", canvasWidth);
 
   function drawDiamond(location, info, cssClass) {
+    cssClass = cssClass || 'diamondShape';
+
     var x = location[0],
         y = location[1];
 
@@ -18,8 +20,8 @@
         right  = (x + dWidth/2).toString() + "," + (y + dHeight/2).toString(),
         bottom = x + "," + (y+dHeight).toString(),
         left   = (x - dWidth/2).toString() + "," + (y + dHeight/2).toString(),
-        cssClass = cssClass || 'diamondShape',
         points = top + " " + right + " " + bottom + " " + left;
+
 
     var big_text = info[0],
         career   = info[1],
@@ -50,139 +52,128 @@
   }
 
   function drawOnBoard(diamondsToBuild) {
-  var diamondsToBuild = [
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
-    [['HR','503','5','2']],
+    diamondsToBuild = [
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
+      [['HR','503','5','2']],
 
-    ];
+      ];
 
-  var upMost      = [0, 0],
-      rightMost   = [0, 0],
-      downMost    = [0, 0],
-      leftMost    = [0, 0],
-      markerLeft  = [0, 0],
-      markerRight = [0, 0];
+    var diamondCount = 0,
+        upMost      = [0, 0],
+        rightMost   = [0, 0],
+        downMost    = [0, 0],
+        leftMost    = [0, 0],
+        markerLeft  = [0, 0],
+        markerRight = [0, 0];
 
-  var diamondCount = 0;
-  while (diamondCount < diamondsToBuild.length) {
-    // Place one diamond.
-    // Set topMost, leftMost, rightMost, downMost.
-    if (diamondCount == 0) {
-      drawDiamond(
-        [450, 0],
-        diamondsToBuild[diamondCount][0],
-        diamondsToBuild[diamondCount][1]
-      );
-      diamondCount += 1;
+    while (diamondCount < diamondsToBuild.length) {
+      // Place one diamond.
+      // Set upMost, leftMost, rightMost, downMost.
+      if (diamondCount === 0) {
+        drawDiamond(
+          [450, 0],
+          diamondsToBuild[diamondCount][0],
+          diamondsToBuild[diamondCount][1]
+        );
+        diamondCount += 1;
 
-      upMost    = [450, 0];
-      rightMost = [upMost[0] + dWidth/2, dHeight/2],
-      downMost  = [upMost[0], dHeight],
-      leftMost  = [upMost[0] - dWidth/2, dHeight/2];
-    } else {
-      // Place next diamond at leftMost.
-      // Set new leftMost
-      // Set new markerLeft at diamond's right point
-      //
-      drawLeftMost();
+        upMost    = [450, 0];
+        rightMost = [upMost[0] + dWidth/2, dHeight/2];
+        downMost  = [upMost[0], dHeight];
+        leftMost  = [upMost[0] - dWidth/2, dHeight/2];
+      } else {
+        // Place next diamond at leftMost.
+        // Set new leftMost
+        // Set new markerLeft at diamond's right point
+        //
+        drawLeftMost();
+      }
       function drawLeftMost() {
+        // Place next diamond at leftMost position
         drawDiamond(
           leftMost,
           diamondsToBuild[diamondCount][0],
           diamondsToBuild[diamondCount][1]
         );
         diamondCount += 1;
-        console.log('drawLeftMost Drawing ' + diamondCount + 'th diamond');
 
         if (diamondCount == diamondsToBuild.length) {return};
+        // Set new leftMost position
         leftMost   = [leftMost[0] - dWidth/2, leftMost[1] + dHeight/2];
+        // Set new markerLeft at diamond's right point
         markerLeft = [leftMost[0] + dWidth, leftMost[1]];
+        // Move on to the right side of the diamond
         drawRightMost();
       }
 
-      // Place next diamond at rightMost.
-      // Set new rightMost
-      // Set new markerRight at diamond's left point
-      //
       function drawRightMost() {
+        // Place next diamond at rightMost position
         drawDiamond(
           rightMost,
           diamondsToBuild[diamondCount][0],
           diamondsToBuild[diamondCount][1]
         );
         diamondCount += 1;
-        console.log('drawRightMost Drawing ' + diamondCount + 'th diamond');
 
         if (diamondCount == diamondsToBuild.length) {return};
+        // Set new rightMost position
         rightMost   = [rightMost[0] + dWidth/2, rightMost[1] + dHeight/2];
+        // Set new markerRight at diamond's left point
         markerRight = [rightMost[0] - dWidth, rightMost[1]];
 
-        // return rightMost, markerRight, diamondCount;
+        // Move on to the left side of the diamond
         drawMarkerLeft();
-      }
-
-
-
-      // Place next diamond at markerLeft
-      //   If top point of diamond == downMost then we're done with this row
-      //   Else set markerLeft at diamond's right point
-      //
-
-      function drawMarkerLeft() {
-        drawDiamond(
-          markerLeft,
-          diamondsToBuild[diamondCount][0],
-          diamondsToBuild[diamondCount][1]
-        );
-        diamondCount += 1;
-        console.log('drawMarkerLeft Drawing ' + diamondCount + 'th diamond');
-
-        if (diamondCount == diamondsToBuild.length) {return};
-
-        if (markerLeft[0] == downMost[0] && markerLeft[1] == downMost[1]) {
-          console.log(diamondCount + 'th diamond ends this row, starting a new one');
-          downMost = [downMost[0], downMost[1]+ dWidth]
-          drawLeftMost();
-        } else {
-          markerLeft = [markerLeft[0] + dWidth/2, markerLeft[1] + dHeight/2];
-          console.log(markerLeft);
-          console.log(downMost);
-          drawMarkerRight();
         }
+        function drawMarkerLeft() {
+          // Place next diamond at the markerLeft position
+          drawDiamond(
+            markerLeft,
+            diamondsToBuild[diamondCount][0],
+            diamondsToBuild[diamondCount][1]
+          );
+          diamondCount += 1;
+
+          if (diamondCount == diamondsToBuild.length) {return};
+
+          // If the top point of this diamond == downMost then we're done with this row
+          if (markerLeft[0] == downMost[0] && markerLeft[1] == downMost[1]) {
+            downMost = [downMost[0], downMost[1]+ dWidth]
+            drawLeftMost();
+          } else {
+            // We keep going and set a new markerLeft position
+            markerLeft = [markerLeft[0] + dWidth/2, markerLeft[1] + dHeight/2];
+            // Head over to the right side of the diamond
+            drawMarkerRight();
+          }
+        function drawMarkerRight() {
+          // Place next diamond at the markerRight position
+          drawDiamond(
+            markerRight,
+            diamondsToBuild[diamondCount][0],
+            diamondsToBuild[diamondCount][1]
+          );
+          diamondCount += 1;
+
+          if (diamondCount == diamondsToBuild.length) {return};
+          // Set new markerRight position
+          markerRight = [markerRight[0] - dWidth/2, markerRight[1] + dHeight/2];
+
+          // Head back over to the left side of the diamond
+          drawMarkerLeft();
+        }
+
       }
-
-      function drawMarkerRight() {
-        drawDiamond(
-          markerRight,
-          diamondsToBuild[diamondCount][0],
-          diamondsToBuild[diamondCount][1]
-        );
-        diamondCount += 1;
-        console.log('drawMarkerRight Drawing ' + diamondCount + 'th diamond');
-
-        if (diamondCount == diamondsToBuild.length) {return};
-        markerRight = [markerRight[0] - dWidth/2, markerRight[1] + dHeight/2];
-
-        drawMarkerLeft();
-        // return markerLeft, diamondCount;
-      }
-
-    };
-  }
-  }
+    }
+    }
 
   drawOnBoard();
-  //   drawDiamond([450,50], ['HR','500','123','1']);
-  //   drawDiamond([350,150],['HR','501','124','1']);
-  //   drawDiamond([550,150],['HR','502','125','1']);
-  //   drawDiamond([450,250],['HR','503','126','2']);
 
 })();
